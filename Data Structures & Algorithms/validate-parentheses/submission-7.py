7416d62262fd30b3b45910e8213bd05fd693c463
Noah Moore <fireballer1000@gmail.com>
2026-06-28 09:10:42 -0500

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        cto = {')':'(',']':'[','}':'{'}
+
+        for i in s:
+            if i in cto:
+                if stack and cto[i] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
